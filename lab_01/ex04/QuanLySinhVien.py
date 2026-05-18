79ef15c499dceb5db4577727d8c94be294e88f41
Sang Huỳnh <sanh7181@gmail.com>
2026-05-18 22:56:12 +0700

    [add] lab_01 ex04

--- lab_01/ex04/QuanLySinhVien.py
+++ lab_01/ex04/QuanLySinhVien.py
@@ -3,7 +3,7 @@ class QuanLySinhVien:
     listSinhVien = []
     def generateID(self):
         maxId = 1
-        if (self.soluongSinhVien()>0):
+        if (self.soLuongSinhVien()>0):
             maxId = self.listSinhVien[0]._id
             for sv in self.listSinhVien:
                 if (maxId < sv._id):
@@ -35,3 +35,57 @@ class QuanLySinhVien:
             self.xepLoaiHocLuc(sv)
         else:
             print("Sinh vien co id = {} khong ton tai.", format(ID))
+    def sortByID(self):
+        self.listSinhVien.sort(key=lambda x: x._id, reverse=False)
+    def sortByName(self):
+        self.listSinhVien.sort(key=lambda x: x._name, reverse=False)
+    def sortByDiemTB(self):
+        self.listSinhVien.sort(key=lambda x: x._diemTB, reverse=False)
+    def findByID(self, ID):
+        searchResult = None
+        if (self.soLuongSinhVien() > 0):
+            for sv in self.listSinhVien:
+                if (sv._id == ID):
+                    searchResult = sv
+        return searchResult
+
+    def findByName(self, keyword):
+        listSV = []
+        if (self.soLuongSinhVien() > 0):
+            for sv in self.listSinhVien:
+                if (keyword.upper() in sv._name.upper()):
+                    listSV.append(sv)
+        return listSV
+
+    
+    def deleteById(self, ID):
+        isDeleted = False
+        sv = self.findByID(ID)
+        if (sv != None):
+            self.listSinhVien.remove(sv)
+            isDeleted = True
+        return isDeleted
+
+    def xepLoaiHocLuc(self, sv:SinhVien):
+        if (sv._diemTB >= 8):
+            sv._hocLuc = "Gioi"
+        elif (sv._diemTB >= 6.5):
+            sv._hocLuc = "Kha"
+        elif (sv._diemTB >= 5):
+            sv._hocLuc = "Trung binh"
+        else:
+            sv._hocLuc = "Yeu"
+
+    def showSV(self, listSV):
+        print("{:<8} {:<18} {:<8} {:<8}{:<8} {:<8}"
+            .format("ID", "Name", "Sex", "Major", "Diem TB", "Hoc Luc"))
+        if (listSV.__len__() > 0):
+            for sv in listSV:
+                print("{:<8} {:<18} {:<8} {:<8}{:<8} {:<8} "
+                    .format(sv._id, sv._name, sv._sex, sv._major,
+                    sv._diemTB, sv._hocLuc))
+        print("\n")
+
+    def getListSinhVien(self):
+        return self.listSinhVien
+
