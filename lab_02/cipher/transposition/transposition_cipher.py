a90344dba5d5d864e67bbc28a8662785434d45b1
Sang Huỳnh <sanh7181@gmail.com>
2026-05-21 10:13:42 +0700

    [add] lab02 transposition

--- lab_02/cipher/transposition/transposition_cipher.py
+++ lab_02/cipher/transposition/transposition_cipher.py
@@ -1,12 +1,23 @@
-class TraspositionCipher:
+class TranspositionCipher:
     def __init__(self):
         pass
 
-    def transposition_encrypt(self, plain_text, key):
-        cipher_text = [''] * key
-        for column in range(key):
-            pointer = column
-            while pointer < len(plain_text):
-                cipher_text[column] += plain_text[pointer]
+    def encrypt(self, text, key):
+        encrypted_text = ''
+        for col in range(key):
+            pointer = col
+            while pointer < len(text):
+                encrypted_text += text[pointer]
                 pointer += key
-        return ''.join(cipher_text)
+        return encrypted_text
+
+    def decrypt(self, text, key):
+        decrypted_text = [''] * key
+        row, col = 0, 0
+        for symbol in text:
+            decrypted_text[col] += symbol
+            col += 1
+            if col == key or (col == key - 1 and row >= len(text) % key):
+                col = 0
+                row += 1
+        return ''.join(decrypted_text)
